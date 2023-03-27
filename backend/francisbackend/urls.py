edc1961d19a757073909e8de921f342744c21214
coninegaming <coninegaming@gmail.com>
2023-03-27 20:24:38 +0800

    first commit

--- backend/francisbackend/urls.py
+++ backend/francisbackend/urls.py
@@ -20,10 +20,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 
-from base.views import index
+# from base.views import index
 
 urlpatterns = [
-    path('', index, name='index'),
+    # path('', index, name='index'),
     path('admin/', admin.site.urls),
     path('api/', include('base.urls')),
  
